3c24fc49f74633aa8758f60eff1b16ebef8fc8e1
andreW8080 <bugs101@hotmail.co.uk>
2026-01-28 16:38:29 +0000

    Update requirements

--- db.py
+++ db.py
@@ -1,8 +1,12 @@
+import os
 from typing import AsyncGenerator
+from dotenv import load_dotenv
+
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "sqlite+aiosqlite:///./app.db"
+load_dotenv()
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 
